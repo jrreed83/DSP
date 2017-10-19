9ae36f9944ec643312f01cb9a7143d7a537fb60e
jrreed83 <joeyrreed@gmail.com>
2017-10-19 05:29:28 -0700

    jrr

--- DSP.hs
+++ DSP.hs
@@ -5,26 +5,39 @@ module DSP where
                                  , nSymbols         :: Int 
                                  } deriving (Show) 
 
-    
+    numSamples :: FilterSpec -> Int
+    numSamples spec
+         = (m * n) + 1
+         where m = (samplesPerSymbol spec)
+               n = (nSymbols spec)
 
-    sinc :: Float -> Float
+    basic :: FilterSpec
+    basic = FilterSpec 16 0.5  5
+
+    sinc :: Double -> Double
     sinc 0.0 = 1.0
     sinc x   = (sin x) / x
 
-    sincN :: Float -> Float
+    sincN :: Double -> Double
     sincN x = sinc (pi * x)
 
-    raisedCosine :: FilterSpec -> Int -> Float
-    h spec i =  
-        if i == (sps / twoBeta) then
+    raisedCosine :: FilterSpec -> Int -> Double
+    raisedCosine spec i =  
+        if abs(iD) == ( spsD / twoBeta) then
             (pi/4) * sincN(1/twoBeta)
         else 
-            5.0
-        end
+            sincN(xi) * cos(pi*beta*xi)/(1.0 -(2.0*beta*xi)^2)
         where beta    = rollOff spec
               twoBeta = 2 * beta
               sps     = samplesPerSymbol spec
-
+              iD      = (fromIntegral i)
+              spsD    = (fromIntegral sps)
+              xi      = iD/spsD
+    
+    design :: FilterSpec -> [Double]
+    design spec = 
+        [h i | i <- [-5..5]]
+        where h = raisedCosine spec
 --    raisedCosine :: FilterSpec -> [Float]
 --    raisedCosine spec = 
 --        let x0 = 1
